e8629c25cbd62b62a6da4c95195917eebca8b7f7
Elton Garbin <eltongarbin@gmail.com>
2019-05-12 12:19:13 -0300

    styling notfound page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
-
+import React, { PureComponent } from 'react';
+import { Typography, Button } from '@material-ui/core';
+import styled from 'styled-components';
 import { withPageLayout } from 'components/PageLayout';
 
-function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 50px;
+`;
+
+class PageNotFound extends PureComponent {
+  handleBackClick = () => {
+    this.props.history.goBack();
+  };
+
+  render() {
+    return (
+      <Container>
+        <Typography variant="h6" gutterBottom>
+          Sorry, this page isn't here.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.handleBackClick}
+        >
+          Voltar
+        </Button>
+      </Container>
+    );
+  }
 }
 
-export default withPageLayout({ title: 'Page not found (404)' })(NotFoundPage);
+export default withPageLayout({ title: 'Page not found (404)' })(PageNotFound);
